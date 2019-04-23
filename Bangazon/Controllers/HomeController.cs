0e5dc34c9761717734928206a7f9ca8b2684bd05
unknown <huntermettsdev@gmail.com>
2019-04-23 14:16:56 -0500

    consistant details with nick

--- Bangazon/Controllers/HomeController.cs
+++ Bangazon/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Bangazon.Controllers
         // GET: Products
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Product.Include(p => p.ProductType).Include(p => p.User).Take(20).Reverse();
+            var applicationDbContext = _context.Product.Include(p => p.ProductType).Include(p => p.User).Take(20);
             return View(await applicationDbContext.ToListAsync());
         }
 
